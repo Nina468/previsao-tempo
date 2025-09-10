6d1230ae742e69e5b5707bfe42de40307148ef6e
Nina468 <anaclarapereirah468@gmail.com>
2025-09-10 09:39:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let chave = "7f267cf6130eb9a94f198d2f5bb46f57"
+
+function clique(){
+    let cidade = document.querySelector(".input-cidade").value 
+    
+    buscarCidade(cidade);
+} 
+
+
+
+
+
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+async function buscarCidade(cidade)  {
+       let dados = await fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q="+
+    cidade+
+   "&appid="+
+   chave +
+   "&lang=pt_br" +
+   "&units=metric")
+    .then(res =>res.json())
+
+    console.log(dados);
+    colocaNaTela(dados);}
+
+    function colocaNaTela(dados){
+    document.querySelector(".nome-cidade") .innerHTML = dados.name;
+    document.querySelector(".temp").innerHTML = Math.round(dados.main.temp) + "°C";
+    document.querySelector(".descrição").innerHTML = "Descrição: " + dados.weather[0].description;
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%";
+}
+
+
+
+
